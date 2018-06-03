330ebc927cf28532402ade4b633ce68d28f4ce25
mikezhen <mike.zhen@icloud.com>
2018-06-03 00:38:11 -0400

    Removed unused variable to fix linting error

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import ScrollUp from 'react-scroll-up';
 import rocket from 'assets/img/rocket.png';
 import './Footer.css';
